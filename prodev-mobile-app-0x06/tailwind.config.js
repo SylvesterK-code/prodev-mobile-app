d9664bbfc0a4b821fa9c64129cb90836c8d302ca
sylvesterK-code <sylvesterkeystarling@gmail.com>
2025-09-07 22:47:23 +0000

    2. Styling with NativeWind (TailwindCSS)

--- prodev-mobile-app-0x06/tailwind.config.js
+++ prodev-mobile-app-0x06/tailwind.config.js
@@ -11,11 +11,32 @@
 
 
 
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./app/**/*.{js,jsx,ts,tsx}",
+//     "./components/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+
+
+
+
+
+
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./App.{js,jsx,ts,tsx}",
     "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
